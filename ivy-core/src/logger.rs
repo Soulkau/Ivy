035a1438a27929fcf09c1b5e108261b57f0d7a98
Soulkau <156959788+Soulkau@users.noreply.github.com>
2026-08-12 13:15:56 +0300

    feat(logs): send logs via mqtt

---
+++ ivy-core/src/logger.rs
@@ -0,0 +1,51 @@
+use core::sync::atomic::{AtomicU32, Ordering};
+
+use embassy_sync::{
+    blocking_mutex::raw::{CriticalSectionRawMutex, NoopRawMutex},
+    channel::Channel,
+};
+use talky::logs::{BufVisitor, DeviceLog, LogLevel};
+use tracing::{Level, Subscriber, span};
+
+pub static LOG_CHANNEL: Channel<CriticalSectionRawMutex, DeviceLog, 16> = Channel::new();
+
+pub struct SendLogger {
+    max_level: Level,
+    next_id: AtomicU32,
+}
+
+impl SendLogger {
+    pub fn new(max_level: Level) -> Self {
+        Self {
+            max_level,
+            next_id: AtomicU32::new(0),
+        }
+    }
+}
+
+impl Subscriber for SendLogger {
+    fn enabled(&self, metadata: &tracing::Metadata<'_>) -> bool {
+        metadata.level() <= &self.max_level
+    }
+
+    fn event(&self, event: &tracing::Event<'_>) {
+        let mut visitor = BufVisitor::new();
+        event.record(&mut visitor);
+
+        let log = DeviceLog::new(LogLevel::from(event.metadata().level()), visitor.buf);
+        LOG_CHANNEL.try_send(log).ok();
+    }
+
+    fn new_span(&self, span: &span::Attributes<'_>) -> span::Id {
+        let id = self.next_id.fetch_add(1, Ordering::Relaxed);
+        span::Id::from_u64(id as u64)
+    }
+
+    fn record(&self, span: &span::Id, values: &span::Record<'_>) {}
+
+    fn exit(&self, span: &span::Id) {}
+
+    fn enter(&self, span: &span::Id) {}
+
+    fn record_follows_from(&self, span: &span::Id, follows: &span::Id) {}
+}
--- ivy-core/src/mqtt.rs
+++ ivy-core/src/mqtt.rs
@@ -1,18 +1,23 @@
 use core::{marker::PhantomData, net::Ipv4Addr};
 
-use embassy_futures::join::join;
+use embassy_futures::{
+    join::join,
+    select::{Either, select},
+};
 use embassy_net::{
     Stack,
     tcp::client::{TcpClient, TcpClientState},
 };
 use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
 use embedded_tls::{Aes128GcmSha256, CryptoRng, CryptoRngCore, TlsConfig, UnsecureProvider};
-use ivy_types::Runnable;
+use ivy_types::actor::Actor;
 use mqttrust::{
-    Config, IpBroker, State,
+    Config, IpBroker, Publish, State, Subscribe,
     transport::embedded_tls::{TlsNalTransport, TlsState},
 };
 use static_cell::StaticCell;
+
+use crate::logger::LOG_CHANNEL;
 
 const MAX_NET_PAYLOAD_SIZE: usize = 4096;
 
@@ -22,14 +27,18 @@ const TLS_BUFFER_SIZE: usize = 16640;
 
 const _: () = assert!(MAX_NET_PAYLOAD_SIZE + 500 <= TLS_BUFFER_SIZE);
 
-#[derive(Clone)]
 pub struct MqttModule<Rng: CryptoRngCore + 'static> {
     stack: Stack<'static>,
-    trng: Rng,
+    trng: Option<Rng>,
 }
 
-impl<Rng: CryptoRngCore> Runnable for MqttModule<Rng> {
-    async fn run(self) -> ! {
+impl<Rng: CryptoRngCore> MqttModule<Rng> {
+    pub fn new(stack: Stack<'static>, trng: Rng) -> Self {
+        Self { stack, trng: Some(trng) }
+    }
+
+    pub async fn run(&mut self) -> ! {
+        tracing::info!("[MqttModule] Running MQTT module");
         static MQTT_STATE: StaticCell<State<CriticalSectionRawMutex, MAX_NET_PAYLOAD_SIZE, MAX_NET_PAYLOAD_SIZE>> = StaticCell::new();
         let state = MQTT_STATE.init(State::new());
         let configuration = Config::builder()
@@ -37,33 +46,57 @@ impl<Rng: CryptoRngCore> Runnable for MqttModule<Rng> {
             .password(option_env!("NATS_PASS").unwrap().as_ref())
             .username(option_env!("NATS_USER").unwrap().as_ref())
             .build();
+        tracing::info!("[MqttModule] MQTT configuration built");
+
         let (mut mqtt_stack, client) = mqttrust::new(state, configuration);
+        tracing::info!("[MqttModule] MQTT stack created");
 
         let stack = self.stack;
+        tracing::info!("Staring future 1");
+
+        tracing::info!("[MqttModule] Configurating MQTT transport");
+        let broker = IpBroker::new(Ipv4Addr::new(217, 195, 48, 206), 1883);
+        let tls_config = TlsConfig::new().enable_rsa_signatures();
+        let tcp_state = TcpClientState::<1, TCP_BUFFER_SIZE, TCP_BUFFER_SIZE>::new();
+        let network = TcpClient::<'_, 1, TCP_BUFFER_SIZE, TCP_BUFFER_SIZE>::new(stack, &tcp_state);
+        let tls_state = TlsState::<TLS_BUFFER_SIZE, TLS_BUFFER_SIZE>::new();
+        let provider = UnsecureProvider::new::<Aes128GcmSha256>(self.trng.take().expect("This should never fail"));
+        let mut transport = TlsNalTransport::new(&network, broker, &tls_state, &tls_config, provider);
+        tracing::info!("[MqttModule] MQTT transport created");
         let mqtt_task = async move {
-            let broker = IpBroker::new(Ipv4Addr::new(217, 195, 48, 206), 1883);
-            let tls_config = TlsConfig::new().enable_rsa_signatures();
-            let tcp_state = TcpClientState::<1, TCP_BUFFER_SIZE, TCP_BUFFER_SIZE>::new();
-            let network = TcpClient::<'_, 1, TCP_BUFFER_SIZE, TCP_BUFFER_SIZE>::new(stack, &tcp_state);
-            let tls_state = TlsState::<TLS_BUFFER_SIZE, TLS_BUFFER_SIZE>::new();
-            let provider = UnsecureProvider::new::<Aes128GcmSha256>(self.trng);
-            let mut transport = TlsNalTransport::new(&network, broker, &tls_state, &tls_config, provider);
             loop {
                 mqtt_stack.run(&mut transport).await
             }
         };
 
         let client_task = async move {
+            let topics = ["mushclim/listen".into()];
+            let mut subscription = client.subscribe::<1>(Subscribe::builder().topics(&topics).build()).await.unwrap();
+
+            tracing::info!("[MqttModule] Running MQTT client task");
+
             loop {
-                let _ = client;
+                let log_sending_task = async {
+                    let mut work_buf = [0u8; 256];
+                    let log = LOG_CHANNEL.receive().await;
+                    let serialized = postcard::to_slice(&log, &mut work_buf).unwrap();
+                    client
+                        .publish(Publish::builder().topic_name("mushclim/log").payload(&*serialized).qos(mqttrust::QoS::AtLeastOnce).build())
+                        .await
+                        .unwrap();
+                };
+
+                match select(subscription.next_message(), log_sending_task).await {
+                    Either::First(Some(msg)) => {}
+                    Either::Second(_) => {}
+                    _ => {}
+                }
             }
         };
-        join(client_task, mqtt_task).await.0
-    }
-}
 
-impl<Rng: CryptoRngCore + 'static> MqttModule<Rng> {
-    pub fn new(stack: Stack<'static>, trng: Rng) -> Self {
-        MqttModule { stack, trng }
+        join(client_task, mqtt_task).await;
+        tracing::info!("Starting future 2, we join them duh");
+
+        unreachable!()
     }
 }
