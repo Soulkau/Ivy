ecf00a646230c116be9212c49d3037542714b46a
Soulkau <156959788+Soulkau@users.noreply.github.com>
2026-09-07 21:13:54 +0300

    feat(lib): add mk_static helper macro

--- ivy-core/src/lib.rs
+++ ivy-core/src/lib.rs
@@ -13,3 +13,11 @@ pub use ivy_types as types;
 pub use ivy_types::actor;
 
 pub use paste;
+
+#[macro_export]
+macro_rules! mk_static {
+    ($t:ty, $val:expr) => {{
+        static STATIC_CELL: static_cell::StaticCell<$t> = static_cell::StaticCell::new();
+        STATIC_CELL.init(($val))
+    }};
+}
